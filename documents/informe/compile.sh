fac17d4db993e3993cea15cdf4e0c66f8d95d0bb
Anel Ademovic <asuljic48@gmail.com>
2025-12-14 19:24:53 +0100

    Extendre 01_identificacio.tex

--- documents/informe/compile.sh
+++ documents/informe/compile.sh
@@ -1,2 +1,6 @@
 mkdir -p out
 pdflatex -output-directory=out main.tex
+pdflatex -output-directory=out main.tex
+pdflatex -output-directory=out main.tex
+pdflatex -output-directory=out main.tex
+pdflatex -output-directory=out main.tex
