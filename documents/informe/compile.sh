a3eb25d3ca733791505ae72e34b49767a547d8af
Anel Ademovic <asuljic48@gmail.com>
2025-12-14 21:53:19 +0100

    Afegir 04_implementacio.tex

--- documents/informe/compile.sh
+++ documents/informe/compile.sh
@@ -1,6 +1,6 @@
 mkdir -p out
-pdflatex -output-directory=out main.tex
-pdflatex -output-directory=out main.tex
-pdflatex -output-directory=out main.tex
-pdflatex -output-directory=out main.tex
-pdflatex -output-directory=out main.tex
+lualatex -output-directory=out main.tex
+lualatex -output-directory=out main.tex
+lualatex -output-directory=out main.tex
+lualatex -output-directory=out main.tex
+lualatex -output-directory=out main.tex
